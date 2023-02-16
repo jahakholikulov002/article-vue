e0bbebe48b86038a77de541920c9f43e2dd6b40f
jakhongirkholikulov <98052598+jahakholikulov002@users.noreply.github.com>
2023-02-16 16:36:36 +0500

    app created

---
+++ src/components/app/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="main">
+    <h1>hello world</h1>
+  </div>
+</template>
+<script>
+export default {
+ 
+  
+}
+</script>
+<style scoped> 
+  .main{
+    position: relative;
+    width: 100%;
+    height: 100vh;
+  }
+</style>
---
+++ src/main.js
@@ -0,0 +1,11 @@
+import { createApp } from 'vue'
+import App from './components/app/App.vue'
+// import router from './router'
+
+// import './assets/main.css'
+
+const app = createApp(App)
+
+// app.use(router)
+
+app.mount('#app')
